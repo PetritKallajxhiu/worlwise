1b01256ec751384545ff4d475239f0270cd5b491
Petrit Kallajxhiu <Petrit.k@vikartech.com>
2025-03-09 22:42:17 +0100

    first commit

---
+++ src/component/Map.jsx
@@ -0,0 +1,30 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
+function Map() {
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams?.get("lat");
+  const lng = searchParams?.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 40, lng: 50 })}>
+        cahnge pos
+      </button>
+    </div>
+  );
+}
+
+export default Map;
